b88ef218b79b0fe2b2030e6a80086eea5dd63b6f
Raihan Rana <raihanranacz@gmail.com>
2026-02-07 08:45:32 +0100

    fixing code

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,21 +1,21 @@
-from playwright.sync_api import expect
+from playwright.sync_api import expect, Any
 from pages.login_page import LoginPage
 
-def test_login_with_valid_credentials(page):
+def test_login_with_valid_credentials(page: Any):
     login = LoginPage(page)
     login.open()
     login.login("Admin", "admin123")
 
     expect(page).to_have_url_containing("/dashboard")
 
-def test_login_with_empty_fields(page):
+def test_login_with_empty_fields(page: Any):
     login = LoginPage(page)
     login.open()
     login.login("", "")
 
     expect(login.required_error).to_have_count(2)
 
-def test_login_with_wrong_credentials(page: Page):
+def test_login_with_wrong_credentials(page: Any):
     login = LoginPage(page)
 
     login.open()
